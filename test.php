57b7138e180413702dc629518b4513a2677963a3
VoidWalker <sohanoleksa@gmail.com>
2016-02-28 23:32:16 +0200

    Abstract

--- test.php
+++ test.php
@@ -1,2 +1,26 @@
 <?php
-phpinfo();
+
+abstract class aClass
+{
+    public $var = 10;
+
+    public function testFunction(){
+        return 11;
+    }
+
+    public function testPrivet()
+    {
+        return 10;
+    }
+}
+
+class B extends aClass
+{
+    public function testFunction(){
+
+    }
+}
+
+$testB = new B();
+$testB->testPrivet();
+$testB->var;
